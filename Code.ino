d2a1fefc75563c9aa25b16ff7e2a16b41ba3f52d
Onkar Waman <onkarwaman5@gmail.com>
2022-04-29 09:56:35 +0530

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -18,7 +18,7 @@ int contrast=100; /*It is the contrast of display,
    used pin 5 for contrast and pin 6 for backlight brightness*/
    
 int backlight=10; /*It is the brightness of display, 
-                    always keep it 60 or less, 
+                    always keep it in range of 0 to 60, 
                     if increased more than 60,
                     the display will burn out.*/
 
